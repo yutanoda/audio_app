bbe342e05912b4ecb5904bcdbee546d9518f83b1
y-noda <y-noda@entaworks.co.jp>
2021-05-21 18:33:18 +0900

    25

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 
@@ -28,6 +29,14 @@ Route::get('posts/{post:slug}', function (Post $post) { //Post::where('slug', $p
 
     return view('post', [
         'post' => $post
+    ]);
+
+});
+
+Route::get('categories/{category:slug}', function (Category $category) { 
+
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 
 });
