d4f31e16d4f49326d76f2a9b57a44eb816e106a0
y-noda <y-noda@entaworks.co.jp>
2021-05-22 12:08:57 +0900

    27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = \App\Models\User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => "<p>I’m 'Lorem ipsum dolar sit amet.</p>"
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-second-post',
+            'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+            'body' => "<p>I’m 'Lorem ipsum dolar sit amet.</p>"
+        ]);
+
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
                 <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }} </a>
             </p>
             <div>
-                {{ $post->excerpt }} 
+                {!! $post->excerpt !!} 
             </div>
         </article>
     @endforeach
