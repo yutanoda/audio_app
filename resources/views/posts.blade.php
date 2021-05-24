5be8fd8a2b1ffe63b97263d961f0b53b51eba69e
y-noda <y-noda@entaworks.co.jp>
2021-05-24 19:22:09 +0900

    31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,21 @@
 <x-layout>
+    @include ('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
+        <x-post-featured-card />
+        
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+    {{--
     @foreach ($posts as $post) 
         <article>
             <h1>
@@ -14,6 +31,7 @@
             </div>
         </article>
     @endforeach
+    --}}
 </x-layout>
 
 
