8594e93be585bf7d978d4c949e6abfcbd8af3eff
y-noda <y-noda@entaworks.co.jp>
2021-05-25 18:12:32 +0900

    34

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,9 @@ Route::get('/', function () {
     });
 
     return view('posts', [
-        'posts' => Post::latest()->get() //associated in each method
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
+         //associated in each method
     ]);
 });
 
@@ -40,15 +42,17 @@ Route::get('posts/{post:slug}', function (Post $post) { //Post::where('slug', $p
 Route::get('categories/{category:slug}', function (Category $category) { 
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
-
 });
 
 Route::get('authors/{author:username}', function (User $author) { 
 
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 
 });
