d644627085ad20283b53865d1207b5735273e144
y-noda <y-noda@entaworks.co.jp>
2021-06-02 19:33:32 +0900

    40 viewのクラス

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,16 +11,14 @@ class PostController extends Controller
     public function index()
     {
 
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get(), 
-            'categories' => Category::all(),
-            'currentCategory' => Category::where('slug', request('category'))->first()
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category']))->get()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
