d7d441e31d7f5f03291d8a86be081be757c8fc1b
y-noda <y-noda@entaworks.co.jp>
2021-06-03 15:53:24 +0900

    41 user 検索も一体化

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('authors/{author:username}', function (User $author) { 
-
-    return view('posts', [
-        'posts' => $author->posts
-    ]);
-
-});
 
