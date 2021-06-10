6fb50eb2679d332de1b1247e382d28162123ce85
y-noda <y-noda@entaworks.co.jp>
2021-06-11 07:45:51 +0900

    formの作成

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,8 +15,13 @@
                   type="text"
                   name="name"
                   id="name"
+                  value="{{ old('name') }}"
                   required
           >
+
+          @error('name')
+            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+          @enderror
         </div>
 
         <div class="mb-6">
@@ -29,8 +34,12 @@
                   type="text"
                   name="username"
                   id="username"
+                  value="{{ old('username') }}"
                   required
           >
+          @error('username')
+            <p class="text-red-500 text-xs mt-1">{{ $message }}
+          @enderror
         </div>
 
         <div class="mb-6">
@@ -43,8 +52,12 @@
                   type="email"
                   name="email"
                   id="email"
+                  value="{{ old('email') }}"
                   required
           >
+          @error('email')
+            <p class="text-red-500 text-xs mt-1">{{ $message }}
+          @enderror
         </div>
 
         <div class="mb-6">
@@ -59,6 +72,9 @@
                   id="password"
                   required
           >
+          @error('password')
+            <p class="text-red-500 text-xs mt-1">{{ $message }}
+          @enderror
         </div>
 
         <div class="mb-6">
