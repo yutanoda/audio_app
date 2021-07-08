ca61d9f810dd01993d74c30e8b21b030a06d8fd3
y-noda <y-noda@entaworks.co.jp>
2021-07-08 17:44:59 +0900

    52

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,18 +19,14 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (auth()->attempt($attributes)) {
-            session()->regenerate();
-            return redirect('/')->with('success', 'Welcom Back!');
+        if (! auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
         }
 
-        throw ValidationException::withMessages([
-            'email' => 'Your provided credentials could not be verified.'
-        ]);
-
-        // return back()
-        //     ->withInput()
-        //     ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+        session()->regenerate();
+        return redirect('/')->with('success', 'Welcom Back!');
     }
 
     public function destroy()
