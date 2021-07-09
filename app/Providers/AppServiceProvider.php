1c8914dd64eef659a910b201e31157945c9ec4e3
y-noda <y-noda@entaworks.co.jp>
2021-07-09 19:40:31 +0900

    56

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::unguard();
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,11 +4,9 @@
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                     <img src="/images/illustration-1.png" alt="" class="rounded-xl">
-
                     <p class="mt-4 block text-gray-400 text-xs">
                         Published <time>{{ $post->created_at->diffForHumans() }}</time>
                     </p>
-
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
@@ -18,7 +16,6 @@
                         </div>
                     </div>
                 </div>
-
                 <div class="col-span-8">
                     <div class="hidden lg:flex justify-between mb-6">
                         <a href="/"
@@ -35,40 +32,47 @@
 
                             Back to Posts
                         </a>
-
                         <div class="space-x-2">
                             <x-category-button :category="$post->category" />
                         </div>
                     </div>
-
                     <h1 class="font-bold text-3xl lg:text-4xl mb-10">
                         {{ $post->title }}
                     </h1>
-
                     <div class="space-y-4 lg:text-lg leading-loose">
                         {!! $post->body !!}
                     </div>
                 </div>
-
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    <x-panel>
-                        <form method="POST" action="#">
-                            @csrf
-                            <header class="flex items-center">
-                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-xl">
+                    @auth
+                        <x-panel>
+                            <form method="POST" action="/posts/{{ $post->slug }}/comments">
+                                @csrf
+                                <header class="flex items-center">
+                                    <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-xl">
+                                    <h2 class="ml-4">Want to participate?</h2>
+                                </header>
+                                <div class="mt-6">
+                                    <textarea 
+                                        name="body" 
+                                        class="w-full text-sm focus:outline-none focus:ring" 
+                                        rows="5" 
+                                        placeholder="Quick, thing of something to say!"></textarea>
+                                </div>
 
-                                <h2 class="ml-4">Want to participate?</h2>
-                            </header>
-
-                            <div class="mt-6">
-                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, thing of something to say!"></textarea>
-                            </div>
-
-                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
-                        </form>
-                    </x-panel>
+                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                    <button type="submit" 
+                                            class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+                                        Post
+                                    </button>
+                                </div>
+                            </form>
+                        </x-panel>
+                    @else
+                        <p class="font-semibold">
+                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Log in</a> to leave a comment
+                        </p>
+                    @endauth
 
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
