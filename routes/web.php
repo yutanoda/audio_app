a887aecba283dd8803a408de1b2dee0383868cac
yutanoda <atuyuta902@gmail.com>
2021-07-25 15:30:56 +0900

    59

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+    request()->validate(['email' => 'required|email']);
+
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -17,12 +19,19 @@ Route::get('ping', function () {
         'server' => 'us6'
     ]);
 
-    $response = $mailchimp->lists->addListMember('a55c4cd99b', [
-        'email_address' => 'y-noda@entaworks.co.jp',
-        'status' => 'subscribed'
-    ]);
+    try {
+        $response = $mailchimp->lists->addListMember('a55c4cd99b', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
 
-    ddd($response);
+    return redirect('/')
+        ->with('success', 'You are signed up for our newsletter!');
 });
 
 /*
